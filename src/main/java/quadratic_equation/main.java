cd8215724375ffeaa6287fbb82352d7191506435
Tigran Poghosyan <tigran_poghosyan@epam.com>
2023-02-27 12:39:59 +0400

    d

--- src/main/java/quadratic_equation/main.java
+++ src/main/java/quadratic_equation/main.java
@@ -1,9 +1,6 @@
 package quadratic_equation;
 
-import org.xml.sax.EntityResolver;
-
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
@@ -13,7 +10,7 @@ public class main {
         double c = scanner.nextDouble();*/
 
         Solution solve1 = new Solution();
-        double[] solution = solve1.solve(0,2,3);
+        double[] solution = solve1.solve(0, 2, 3);
         System.out.println(Arrays.toString(solution));
 
     }
--- src/test/java/quadratic_equation/SolutionTest.java
+++ src/test/java/quadratic_equation/SolutionTest.java
@@ -1,11 +1,17 @@
 package quadratic_equation;
 
+
+
+import org.testng.annotations.Test;
+
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
+
 
 public class SolutionTest {
-    @org.junit.Test
+    @Test
     public void solveAnegative() {
         Solution number = new Solution();
         double[] current = number.solve(-2, 1, 1);
@@ -20,14 +26,14 @@ public class SolutionTest {
 
     }
 
-    @org.junit.Test
+    @Test
     public void solveAzero() {
         Solution number = new Solution();
         double[] current = number.solve(0, 1, -4);
         double extended = 4.0;
         assertEquals(current[0], extended, 0.001);
     }
-        @org.junit.Test
+        @Test
         public void solve2() {
         Solution number = new Solution();
         double[] current = number.solve(1, 2, 1);
@@ -35,7 +41,7 @@ public class SolutionTest {
         assertEquals(current[0], extended, 0.001);
 
 
-    }@org.junit.Test
+    }@Test
         public void nan() {
         Solution number = new Solution();
         double[] current = number.solve(1, 0, 100);
@@ -46,7 +52,7 @@ public class SolutionTest {
     }
 
 
-    @org.junit.Test
+    @Test
         public void solveAllzero() {
         Solution number = new Solution();
         double[] current = number.solve(-0, 0, 0);
